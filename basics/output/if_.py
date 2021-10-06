29c2e7ccbdd1f780b77bc10da8629f32516f4032
shakibul07 <91551683+shakibul07@users.noreply.github.com>
2021-10-06 23:11:16 +0100

    added code to demonstarte the use of an if statement .

---
+++ basics/output/if_.py
@@ -0,0 +1,5 @@
+print("What type of boook is this? ")
+book = input()
+if book == "adventure":
+    print ( "I like adventure books! ")
+print("Finished reading book. ")
---
+++ basics/output/if_else.py
@@ -0,0 +1,10 @@
+print("Please enter the activity to be performed. ")
+print("activity type 1 = 'calculate' or 'not' ")
+activity = input()
+
+if activity == "calculate":
+    print("Performing caluations... ")
+else:
+    print("Performing activity...")
+
+print("Activity completed")
