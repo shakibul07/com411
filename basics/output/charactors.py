b00f3e4bfac8d4869640c8104cea6e7ae433228f
shakibul07 <91551683+shakibul07@users.noreply.github.com>
2021-10-07 12:41:51 +0100

    string index

---
+++ basics/output/charactors.py
@@ -0,0 +1,10 @@
+print("what strange markings do you see ? " )
+print("♯☼⌂╝ℓ")
+var="♯☼⌂╝ℓ"
+print("Identifying....")
+index= 0
+for dex in var:
+    print(f"index {index} : {dex}")
+    index += 1
+
+print("done! ")
