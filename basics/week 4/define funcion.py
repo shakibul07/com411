182b66b1b53c8abf1612d18edd4d7a166183f0b2
shakibul07 <91551683+shakibul07@users.noreply.github.com>
2021-10-13 23:14:24 +0100

    added code to demonstrate the use of a user defined funtion

---
+++ basics/week 4/define funcion.py
@@ -0,0 +1,15 @@
+# # The function
+# def greet_user():
+#     print("Please enter your name")
+#     name = input()
+#     print("Hello", name)
+#
+#
+# # Calls to the function
+# greet_user()
+def listen():
+    print("What sound did i hear ? ")
+    sound= input()
+    print(f"that was a loud {sound} !")
+
+listen()
