ffad8ee1072700d11253905c22a199ccff7e98bb
shakibul07 <91551683+shakibul07@users.noreply.github.com>
2021-10-13 23:21:58 +0100

    added code to demonstrate the use of a user defined function with nested dicitino

---
+++ basics/week 4/fuction with nesting.py
@@ -0,0 +1,10 @@
+def identify():
+    print("Enter a word representing what they see.")
+    rep = input()
+    if rep == "a large boulder":
+        print(" its time to run")
+    else:
+        print("we will be fine ")
+
+identify()
+
